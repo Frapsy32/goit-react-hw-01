2fc3f2ad48fc0daf728b59f23f41828745142052
Dmytro Biloborodko <frapsenko@ukr.net>
2024-07-18 18:52:36 -0300

    react hw1

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+import Profile from './Profile'
+import FriendList from './FriendList'
+import TransactionHistory from './TransactionHistory'
+
+import userData from '../userData.json'
+import friends from '../friends.json'
+import transactions from '../transactions.json'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Profile name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions}/>
+    </>
+  )
+}
+
+export default App
